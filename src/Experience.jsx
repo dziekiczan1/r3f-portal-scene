070e3dc5befed77e6784407373414fada273665f
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-01-01 20:34:54 +0100

    add portal to the scene

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,12 +1,35 @@
-import {OrbitControls} from '@react-three/drei';
+import {useGLTF, OrbitControls, useTexture, Center} from '@react-three/drei';
 
 export default function Experience() {
+    const {nodes} = useGLTF('./model/portal.glb');
+
+    const bakedTexture = useTexture('./model/baked.jpg');
+    bakedTexture.flipY = false;
+
     return <>
-        <color args={['#030202']} attach="background" />
+        <color args={['#030202']} attach="background"/>
         <OrbitControls makeDefault/>
-        <mesh scale={1.5}>
-            <boxGeometry/>
-            <meshNormalMaterial/>
-        </mesh>
+        <Center>
+            <mesh geometry={nodes.baked.geometry}>
+                <meshBasicMaterial map={bakedTexture}/>
+            </mesh>
+            <mesh
+                geometry={nodes.poleLightA.geometry}
+                position={nodes.poleLightA.position}>
+                <meshBasicMaterial color="#ffffe5"/>
+            </mesh>
+            <mesh
+                geometry={nodes.poleLightB.geometry}
+                position={nodes.poleLightB.position}>
+                <meshBasicMaterial color="#ffffe5"/>
+            </mesh>
+            <mesh
+                geometry={nodes.portalLight.geometry}
+                position={nodes.portalLight.position}
+                rotation={nodes.portalLight.rotation}
+            >
+                <meshBasicMaterial color="#ffffff"/>
+            </mesh>
+        </Center>
     </>
 }
