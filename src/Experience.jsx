08e549d27e466b1d8422fe3cc4860b44ff416fab
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-01-01 20:19:40 +0100

    change bg color

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,11 +2,11 @@ import {OrbitControls} from '@react-three/drei';
 
 export default function Experience() {
     return <>
+        <color args={['#030202']} attach="background" />
         <OrbitControls makeDefault/>
         <mesh scale={1.5}>
             <boxGeometry/>
             <meshNormalMaterial/>
         </mesh>
-
     </>
 }
